fcbd1149136361a621d75f439c259a6e3873b7ab
Crag Wolfe <crag@unstructuredai.io>
2023-03-21 23:30:55 -0700

    feat: speedup text partition by file

--- unstructured/partition/text.py
+++ unstructured/partition/text.py
@@ -30,8 +30,9 @@ def partition_text(
     file: Optional[IO] = None,
     text: Optional[str] = None,
     encoding: Optional[str] = "utf-8",
+    chunk_size: Optional[int] = 4096,
 ) -> List[Element]:
-    """Partitions an .txt documents into its constituent elements.
+    """Partitions a text document/file/string into its constituent elements.
     Parameters
     ----------
     filename
@@ -47,37 +48,59 @@ def partition_text(
     # Verify that only one of the arguments was provided
     exactly_one(filename=filename, file=file, text=text)
 
-    if filename is not None:
-        with open(filename, encoding=encoding) as f:
-            try:
-                file_text = f.read()
-            except (UnicodeDecodeError, UnicodeError) as error:
-                raise error
-
-    elif file is not None:
-        file_text = file.read()
-
-    elif text is not None:
-        file_text = str(text)
-
-    file_content = split_by_paragraph(file_text)
-
     elements: List[Element] = []
     metadata = ElementMetadata(filename=filename)
-    for ctext in file_content:
-        ctext = ctext.strip()
 
-        if ctext == "":
-            continue
-        if is_bulleted_text(ctext):
-            elements.append(ListItem(text=clean_bullets(ctext), metadata=metadata))
-        elif is_us_city_state_zip(ctext):
-            elements.append(Address(text=ctext, metadata=metadata))
-        elif is_possible_narrative_text(ctext):
-            elements.append(NarrativeText(text=ctext, metadata=metadata))
-        elif is_possible_title(ctext):
-            elements.append(Title(text=ctext, metadata=metadata))
-        else:
-            elements.append(Text(text=ctext, metadata=metadata))
+    def process_paragraphs(paragraphs: List[str]) -> None:
+        nonlocal elements, metadata
+
+        if isinstance(paragraphs, str):
+            # protect against a string input where each character
+            # would be processed in the loop below
+            paragraphs = [ctext]
+        
+        for ctext in paragraphs:
+            ctext = ctext.strip()
+
+            if ctext == "":
+                continue
+            if is_bulleted_text(ctext):
+                elements.append(ListItem(text=clean_bullets(ctext), metadata=metadata))
+            elif is_us_city_state_zip(ctext):
+                elements.append(Address(text=ctext, metadata=metadata))
+            elif is_possible_narrative_text(ctext):
+                elements.append(NarrativeText(text=ctext, metadata=metadata))
+            elif is_possible_title(ctext):
+                elements.append(Title(text=ctext, metadata=metadata))
+            else:
+                elements.append(Text(text=ctext, metadata=metadata))
+
+    def process_file(file_object):
+        nonlocal chunk
+
+        while True:
+            read_block = file_object.read(chunk_size)
+            chunk = chunk + read_block
+            
+            paragraphs = split_by_paragraph(chunk)
+            # the last paragraph may be incomplete, so process everything except that
+            if read_block == "":
+                # end of the file, just process what you got
+                process_paragraphs(paragraphs)
+                break
+            elif len(paragraphs) > 1:
+                process_paragraphs(paragraphs[:-1])
+                chunk = paragraphs[-1]
+
+    chunk = ""
+    if filename is not None:
+        with open(filename, encoding=encoding) as f:
+            process_file(f)
+
+    elif file is not None:
+        process_file(file)
+
+    elif text is not None:
+        file_text = process_paragraphs(split_by_paragraph(text))
 
     return elements
